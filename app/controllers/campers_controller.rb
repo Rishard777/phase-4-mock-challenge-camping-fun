39e5be4cc69edd7c059ab3d921d41dc6d69b1710
Rishard Davis <rrdavis777@gmail.com>
2022-05-05 13:45:53 -0400

    working GET/campers/id

--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -1,6 +1,16 @@
 class CampersController < ApplicationController
-
+rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_response
     def index
         render json: Camper.all
+    end
+
+    def show
+        camper = Camper.find(params[:id])
+        render json: camper, serializer: CamperWithActivitiesSerializer
+    end
+
+    private
+    def record_not_found_response
+        render json: {error: "Camper not found"}, status: :not_found
     end
 end
