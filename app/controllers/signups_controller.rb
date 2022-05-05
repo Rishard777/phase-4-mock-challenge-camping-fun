e3e18b845cbd704b67636914127c13cd728e224a
Rishard Davis <rrdavis777@gmail.com>
2022-05-05 14:09:01 -0400

    signups routes working

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,2 +1,16 @@
 class SignupsController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    def create
+        sp = Signup.create!(signup_params)
+        render json: sp.activity, status: :created
+    end
+
+    private
+    def signup_params
+        params.permit(:time, :camper_id, :activity_id)
+    end
+
+    def render_unprocessable_entity_response(exception)
+        render json: {errors: exception.record.errors.full_messages}, status: :unprocessable_entity
+    end
 end
