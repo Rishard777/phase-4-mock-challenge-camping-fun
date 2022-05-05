c90e440cd547388c695e606ba50ea10b1373fb74
Rishard Davis <rrdavis777@gmail.com>
2022-05-05 13:56:03 -0400

    create camper working

--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -1,5 +1,6 @@
 class CampersController < ApplicationController
 rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_response
+# rescue_from AcitveRecord::RecordInvalid, with: :render_unprocessable_entity_response
     def index
         render json: Camper.all
     end
@@ -9,8 +10,21 @@ rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_response
         render json: camper, serializer: CamperWithActivitiesSerializer
     end
 
+    def create
+        cm = Camper.create(camper_params)
+        render json: cm, status: :created
+    end
+
     private
     def record_not_found_response
         render json: {error: "Camper not found"}, status: :not_found
     end
+
+    def camper_params
+        params.permit(:name, :age)
+    end
+
+    # def render_unprocessable_entity_response(exception)
+    #     render json: {errors: exception.record.errors.full_messages}, status: :unprocessable_entity
+    # end
 end
